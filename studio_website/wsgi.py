ed019207b1051f82eb5d2cd0b5a60eddf30a23e4
Bdioui Pierre <bdiouipierre@gmail.com>
2022-10-12 15:16:31 +0300

    commit

--- studio_website/wsgi.py
+++ studio_website/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
+from decouple import config
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', f'{config("PROJECT_NAME")}.settings')
 
 application = get_wsgi_application()
